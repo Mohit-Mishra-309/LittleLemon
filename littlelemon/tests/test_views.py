caca4bb9ea139f1d6d72ebdebfc2caa11350869d
mohit <mohitmishra309@outlook.com>
2025-10-06 17:14:49 +0530

    Tested the API urls using insomani, project part 3 commit

---
+++ littlelemon/tests/test_views.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+from restaurant.models import Menu
+
+
+class MenuViewTest(TestCase):
+    def setUp(self):
+        Menu.objects.create(title="Pizza", price=150, inventory=50)
+        Menu.objects.create(title="Burger", price=100, inventory=80)
+        
+    def test_getall(self):
+        response = self.client.get('/menu/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 2)
